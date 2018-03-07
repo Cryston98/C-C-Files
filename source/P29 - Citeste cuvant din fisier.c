4e0c114497f2232e92368af1b27e4cf82197517e
Adrian Deaconu <adriandeaconu98@gmail.com>
2018-03-07 20:29:51 +0200

    Source File Commit

---
+++ source/P29 - Citeste cuvant din fisier.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char name_file[30]="cuvant.txt";
+char cuvant[20];
+
+int main()
+{
+
+    FILE *f;
+    f=fopen(name_file,"r");
+    if(f==NULL)
+    {
+        printf("Eroare deschidere fisier !");
+        exit(1);
+    }
+
+    while(fscanf(f,"%100s",cuvant)!=EOF)
+        {
+          if(cuvant[1]=='l')
+             printf("%s\n",cuvant);
+        }
+    return 0;
+}
